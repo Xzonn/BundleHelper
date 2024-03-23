d225622383bd15d526619e9e0f576737fbed2244
Xzonn <Xzonn@outlook.com>
2024-03-24 00:44:00 +0800

    Update to .NET 8

--- BundleHelper/BundleReader.cs
+++ BundleHelper/BundleReader.cs
@@ -2,7 +2,6 @@
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
 
 namespace BundleHelper
 {
--- BundleHelper/EndianBinaryReader.cs
+++ BundleHelper/EndianBinaryReader.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Buffers.Binary;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
